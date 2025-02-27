cc8eb7fcbf8637f7cffe74dda3d59d1988ab2a1b
Aaron Nayki <138834250+slasherthunder@users.noreply.github.com>
2025-02-26 21:05:26 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
-//riuehuirehuirehiuerhiurehfuiehui
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
