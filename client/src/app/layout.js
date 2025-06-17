a76c2394f44b73fdba315e0bc195bf5cf5d7c0f5
Jer0st <164413660+Jer0st@users.noreply.github.com>
2025-06-17 13:17:01 -0700

    dfsddsfsfds

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "../components/Navigation";
+import Navigation from "./components/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
